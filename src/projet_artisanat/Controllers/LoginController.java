fc6bd6dc52c08373369d7cdb14cc8ddd9ff17b9d
YounesAbounaceur <47831578+YounesAbounaceur@users.noreply.github.com>
2020-06-22 19:04:37 +0100

    Adding the login controller file

---
+++ src/projet_artisanat/Controllers/LoginController.java
@@ -0,0 +1,49 @@
+package projet_artisanat.Controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import projet_artisanat.Models.LigneCommandeEntity;
+import projet_artisanat.Models.UtilisateurEntity;
+import projet_artisanat.ServicesImpl.UtilisateurServiceImpl;
+
+@ManagedBean
+@SessionScoped
+public class LoginController {
+	static UtilisateurEntity utilisater = new UtilisateurEntity();
+	static String message;
+
+	public UtilisateurEntity getUtilisater() {
+		return utilisater;
+	}
+
+	public void setUtilisater(UtilisateurEntity utilisater) {
+		LoginController.utilisater = utilisater;
+	}
+	
+	public String seConnecter() {
+		UtilisateurEntity utilsateurDb = UtilisateurServiceImpl.utilisateurServiceImpl.findByLogin(utilisater.getUtillogin());
+		if(utilsateurDb == null) {
+			message = "L'utilisateur "+utilisater.getUtillogin()+ " n'existe pas";
+		}else if(!utilsateurDb.getUtilpass().equals(utilisater.getUtilpass())) {
+			message =  "Mot de passe incorect";
+		}else {
+			SessionDBUtil.idUser = utilsateurDb.getUtilid();
+			return "index.xhtml";
+		}
+		return "Login";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		LoginController.message = message;
+	}
+	
+	public void modifyQuantity(LigneCommandeEntity lignecommande){
+		
+	}
+	
+}
